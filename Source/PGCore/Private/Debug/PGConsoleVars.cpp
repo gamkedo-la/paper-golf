90c70d399ea8f5602750b389129b9f76ce97f4af
Justin Montgomery <tryfinsigabrt@yahoo.com>
2024-11-01 23:57:13 -0500

    Change all PGConsoleVars to ECVF_Default as they are not render settings

--- Source/PGCore/Private/Debug/PGConsoleVars.cpp
+++ Source/PGCore/Private/Debug/PGConsoleVars.cpp
@@ -23,61 +23,61 @@ namespace PG
 		TEXT("pg.showForces"),
 		false,
 		TEXT("Toggle on/off"),
-		ECVF_Scalability | ECVF_RenderThreadSafe);
+		ECVF_Default);
 
 	TAutoConsoleVariable<bool> CAutomaticVisualLoggerRecording(
 		TEXT("pg.vislog.autorecord"),
 		true,
 		TEXT("Toggle on/off"),
-		ECVF_Scalability | ECVF_RenderThreadSafe);
+		ECVF_Default);
 
 	TAutoConsoleVariable<int32> CStartHoleOverride(
 		TEXT("pg.startHole"),
 		-1,
 		TEXT("Override the start hole number"),
-		ECVF_Scalability | ECVF_RenderThreadSafe);
+		ECVF_Default);
 
 	TAutoConsoleVariable<FString> CStartPositionOverride(
 		TEXT("pg.start.pos"),
 		{},
 		TEXT("Override the player start position"),
-		ECVF_Default); // ECVF_RenderThreadSafe -> This causes editor to not load and then hangs eventually with a crash! 
+		ECVF_Default);
 
 	TAutoConsoleVariable<float> CPlayerAccuracyExponent(
 		TEXT("pg.diff.pAccExp"),
 		-1.0f,
 		TEXT("Adjust client player shot accuracy exponent >= 0"),
-		ECVF_Scalability | ECVF_RenderThreadSafe);
+		ECVF_Default);
 
 	TAutoConsoleVariable<float> CPlayerSpinAccuracyPenalty(
 		TEXT("pg.diff.pSpinAccPen"),
 		-1.0f,
 		TEXT("Adjust client player shot spin accuracy penalty >= 0. Higher is harder"),
-		ECVF_Scalability | ECVF_RenderThreadSafe);
+		ECVF_Default);
 
 	TAutoConsoleVariable<float> CPlayerMaxAccuracy(
 		TEXT("pg.diff.pAccMax"),
 		-1.0f,
 		TEXT("Adjust client player worst case shot accuracy [0,1]. Higher is harder"),
-		ECVF_Scalability | ECVF_RenderThreadSafe);
+		ECVF_Default);
 
 	TAutoConsoleVariable<float> CGlobalPowerAccuracyExponent(
 		TEXT("pg.diff.gAccExp"),
 		-1.0f,
 		TEXT("Adjust all players' shot accuracy exponent >= 1"),
-		ECVF_Scalability | ECVF_RenderThreadSafe);
+		ECVF_Default);
 
 	TAutoConsoleVariable<float> CGlobalPowerAccuracyDampenExponent(
 		TEXT("pg.diff.gPowExp"),
 		-1.0f,
 		TEXT("Adjust all players' accuracy effect on shot power reduction >= 0. Larger is more difficult"),
-		ECVF_Scalability | ECVF_RenderThreadSafe);
+		ECVF_Default);
 
 	TAutoConsoleVariable<float> CGlobalMinPowerMultiplier(
 		TEXT("pg.diff.gPowMin"),
 		-1.0f,
 		TEXT("Adjust all players' min power reduction from accuracy [0,1]"),
-		ECVF_Scalability | ECVF_RenderThreadSafe);
+		ECVF_Default);
 
 	namespace GameMode
 	{
@@ -87,42 +87,42 @@ namespace PG
 			TEXT("pg.mode.allowBots"),
 			-1,
 			TEXT("Override the game mode default allow bots settings -> -1: variable disabled, 0: don't allow bots, 1: allow bots"),
-			ECVF_Scalability | ECVF_RenderThreadSafe
+			ECVF_Default
 		);
 
 		TAutoConsoleVariable<int32> CSkipHumanPlayers(
 			TEXT("pg.mode.skipHumans"),
 			-1,
 			TEXT("Override the game mode default to skip human players -> -1: variable disabled, 0: don't skip all human players, 1: skip all human players"),
-			ECVF_Scalability | ECVF_RenderThreadSafe
+			ECVF_Default
 		);
 
 		TAutoConsoleVariable<int32> CNumDesiredBots(
 			TEXT("pg.mode.numBots"),
 			-1,
 			TEXT("Override the game mode default num bots settings -> -1: variable disabled, 0: don't allow bots, 1: allow bots"),
-			ECVF_Scalability | ECVF_RenderThreadSafe
+			ECVF_Default
 		);
 
 		TAutoConsoleVariable<int32> CNumDesiredPlayers(
 			TEXT("pg.mode.numPlayers"),
 			-1,
 			TEXT("Override the game mode default num players settings -> -1: variable disabled, game starts when numBots + numPlayers join"),
-			ECVF_Scalability | ECVF_RenderThreadSafe
+			ECVF_Default
 		);
 
 		TAutoConsoleVariable<int32> CMinTotalPlayers(
 			TEXT("pg.mode.minPlayers"),
 			-1,
 			TEXT("Override the game mode default min total players setting -> -1: variable disabled. If the players in game falls below this number, the game ends"),
-			ECVF_Scalability | ECVF_RenderThreadSafe
+			ECVF_Default
 		);
 
 		TAutoConsoleVariable<int32> CMaxTotalPlayers(
 			TEXT("pg.mode.maxPlayers"),
 			-1,
 			TEXT("Override the game mode default max total players setting -> -1: variable disabled. Total player count cannot exceed this number"),
-			ECVF_Scalability | ECVF_RenderThreadSafe
+			ECVF_Default
 		);
 	}
 }
